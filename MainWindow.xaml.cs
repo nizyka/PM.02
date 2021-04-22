c07d2819ec52f54c873ff90f17fe2c5782891c33
nizyka <anya.kyazimova@bk.ru>
2021-04-22 08:06:05 +0300

    first

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11ПР
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_auth_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(loginTextBox.Text))
+            {
+                if (!String.IsNullOrEmpty(passwordTextBox.Password))
+                {
+                    IQueryable<Владельцы> владельцы_list = Entities.GetContext().Владельцы.Where(p => p.Логин == loginTextBox.Text && p.Пароль == passwordTextBox.Password);
+                    if (владельцы_list.Count() == 1)
+                    {
+                        MessageBox.Show("Добро пожаловать, " + владельцы_list.First().Имя);
+                    Window1 window = new Window1(владельцы_list.First());
+                    window.Owner = this;
+                    window.Show();
+                    this.Hide();
+                }
+                else MessageBox.Show("Неверный пароль или логин!");
+                }
+            }
+        }
+    }
+
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _11ПР
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private Владельцы владельцы;
+        public Window1(Владельцы владельцы)
+        {
+            InitializeComponent();
+            this.владельцы = владельцы;
+            famili_label.Content = владельцы.Фамилия;
+            name_label.Content = владельцы.Имя;
+            if (!String.IsNullOrEmpty(владельцы.картинка))
+            {
+                try
+                {
+                    Image_profiel.Source = new BitmapImage(new Uri(System.IO.Path.GetFullPath("image/" + владельцы.картинка)));
+                }
+                catch(InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Exit_button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Window_Clossing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.Owner.Show();
+        }
+    }
+
